f60259e2093f728ae5ee3d58bba6869ae12e6254
Clément Picard <44544583+neebul@users.noreply.github.com>
2019-12-12 02:54:27 +0100

    Update activityRecognition_neuralNetwork.R

--- activityRecognition_neuralNetwork.R
+++ activityRecognition_neuralNetwork.R
@@ -1,4 +1,4 @@
-# Libraries needed and working directory 
+# Libraries needed
 library(keras)
 library(tidyverse)
 library(feather)
@@ -7,20 +7,13 @@ library(fastDummies)
 library(e1071)
 library(plyr)
 
-setwd('your_working_directory')
-
 
 
 # Load the data
 df <- read_feather('your_file.feather') %>% 
   mutate(activity_column = fct_drop(activity_column))
 
-data <- df %>% select(-notXorYcategory, -notXorYcategory)
-
-
-
-# Create evaluation variable 
-evaluation <- matrix(0, nrow = length(unique(data$id)), ncol = 1)
+data <- df %>% select(-notXorYcategory, -notXorYcategory)   # just keep y and x values in the dataset
 
 
 
@@ -43,14 +36,14 @@ for (i in unique(data$id)) {
   y_train <- train[, 1]
   
   x_test <- test[, 2:ncol(test)]
-  y_test <- test[, 1]
+  y_test_categories <- test[, 1]
   
   
-  # Reshape y as binary elements (factor -> binary)
+  # Reshape y as one-hot matrices
   y_train <- dummy_cols(y_train, select_columns = "activity")
   y_train <- y_train[, 2:ncol(y_train)]
   
-  y_test <- dummy_cols(y_test, select_columns = "activity")
+  y_test <- dummy_cols(y_test_categories, select_columns = "activity")
   y_test <- y_test[, 2:ncol(y_test)]
   
   
@@ -87,7 +80,7 @@ for (i in unique(data$id)) {
   
   
   
-  # NN - Create the model 
+  # Neural network - Create the model 
   model.nn <- keras_model_sequential() 
   model.nn %>% 
     layer_dense(units = 50, activation = 'relu', input_shape = c(number_of_features), kernel_regularizer = regularizer_l2(l = 0)) %>% 
@@ -104,7 +97,7 @@ for (i in unique(data$id)) {
   
   
   
-  # NN - Train the model 
+  # Neural network - Train the model 
   history <- model.nn %>% fit(
     x_train, y_train, 
     epochs = 30, 
@@ -115,24 +108,24 @@ for (i in unique(data$id)) {
   
   
   
-  # Observe probabilities of predicted outcomes 
+  # Analyze the efficiency of the model
   y_nn_predict <- model.nn %>% predict(x_test)
-  
-  category_nn_predict <- max.col(y_nn_predict)
-  category_nn_predict <- as.factor(category_nn_predict)
+  y_nn_predict_categories <- max.col(y_nn_predict)
+  y_nn_predict_categories <- as.factor(category_nn_predict_categories)
   category_nn_predict <- revalue(category_nn_predict, c("1"="activity1", "2"="activity2", "3"="activity3", "4"="activity4", "5"="activity5"))
   levels(category_nn_predict) <- c("activity1", "activity2", "activity3", "activity4", "activity5")
   
-  category_test <- max.col(y_test)
-  category_test <- as.factor(category_test)
-  category_test <- revalue(category_test, c("1"="activity1", "2"="activity2", "3"="activity3", "4"="activity4", "5"="activity5"))
-  levels(category_test) <- c("activity1", "activity2", "activity3", "activity4", "activity5")
+  caret::confusionMatrix(y_test_categories, y_nn_predict_categories)
   
-  accuracy_nn <- 1 - length(which(category_nn_predict != category_test)) / nrow(test)
- 
-  evaluation[step] <- accuracy_nn
-  
-  errors_nn <- cbind(which(category_nn_predict != category_test), category_nn_predict[which(category_nn_predict != category_test)], 
-                     category_test[which(category_nn_predict != category_test)], y_nn_predict[which(category_nn_predict != category_test),])
 
-  }
+  
+  # Find the errors made by the model
+  indices_errors <- which(category_nn_predict != category_test)
+  errors_categories <- category_nn_predict[indices_errors]
+  true_categories <- category_test[indices_errors]
+  probabilities_errors <- y_nn_predict[indices_errors,]
+  
+  errors_nn <- cbind(indices_errors, errors_categories, true_categories, probabilities_errors)
+  colnames(errors_nn) <- c("index", "error_cat", "true_cat", "p-sitting", "p_lying", "p_standing", "p_walking", "p_running")
+  
+}
